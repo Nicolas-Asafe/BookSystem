8b6b6fcd51060963e62690d15bd6f80a73450666
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-09-07 18:46:40 -0300

    feat: BookService

--- src/main/java/com/bookSystem/service/BookService.java
+++ src/main/java/com/bookSystem/service/BookService.java
@@ -1,10 +1,30 @@
 package com.bookSystem.service;
 
-import com.bookSystem.model.Book;
 import com.bookSystem.model.Author;
+import com.bookSystem.model.Book;
 import java.util.ArrayList;
+import java.util.List;
 
-class BookService{
-    private ArrayList<Book> Books = new ArrayList<>();
+public class BookService{
+    private List<Book> Books = new ArrayList<>();
 
+    public void AddBook(String title,Author author,double price,int year){
+        Book NewBook = new Book(title,author,price,year);
+        this.Books.add(NewBook);
+    }
+    public List<Book> FindBooks(){
+        return Books;
+    }
+    public Book FindOneBookByTitle(String title){
+        String titleOfBookForFind = title;
+        Book BookForFind = null;
+
+        for (Book b : Books){
+            if(b.getTitle().equals(titleOfBookForFind)){
+                BookForFind = b;
+            }
+        }
+
+        return BookForFind;
+    }
 }
