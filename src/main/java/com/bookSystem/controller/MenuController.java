2475b87af6ad1471ceb960c9f46f3ebb1367a042
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-09-10 17:29:40 -0300

    feat: CreateBookOptionUI in menuController

--- src/main/java/com/bookSystem/controller/MenuController.java
+++ src/main/java/com/bookSystem/controller/MenuController.java
@@ -1,42 +1,22 @@
 package com.bookSystem.controller;
 
+import com.bookSystem.model.Author;
+import com.bookSystem.service.BookService;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.function.Supplier;
 
 public class MenuController {
-
-    private final Scanner scanner = new Scanner(System.in);
-
-    private String CreateLayout(ArrayList<String> options, Supplier<String> typeNext) {
+    private BookService bs = new BookService();
+    private void CreateLayout(ArrayList<String> options, Supplier<String> typeNext) {
         System.out.println("========================");
-        List<Integer> opns = new ArrayList<>();
-
         for (int i = 0; i < options.size(); i++) {
-            opns.add(i + 1); // adiciona índice válido
             System.out.println("  " + (i + 1) + " - " + options.get(i));
         }
 
         System.out.println("========================");
+        typeNext.get();
 
-        // converte lista pra array de int
-        int[] opnsArray = opns.stream().mapToInt(Integer::intValue).toArray();
-
-        String response = CurrentResponse(opnsArray, typeNext);
-        return response;
-    }
-
-    private String CurrentResponse(int[] optionsNum, Supplier<String> typeNext) {
-        String response = typeNext.get();
-
-        for (int opn : optionsNum) {
-            if (response.equals(String.valueOf(opn))) {
-                return response; // achou resposta válida
-            }
-        }
-
-        return null; // se não bater com nenhuma opção
     }
 
     public void InitialMenu() {
@@ -44,10 +24,55 @@ public class MenuController {
         options.add("Create book");
         options.add("Find one book");
         options.add("List all books");
-        String response = CreateLayout(options, () -> {
+        CreateLayout(options, () -> {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Select one option: ");
-            return sc.nextLine();
+            while (true) {
+                System.out.print("Select one option: ");
+                String res = sc.nextLine();
+                try {
+                    int num = Integer.parseInt(res);
+                    if (num > 0 && num <= options.size()) {
+                        SelectOptionsMenuInitial(num);
+                        sc.close();
+                        break;
+                    }
+                } catch (NumberFormatException e) {}
+                System.out.println("Try again");
+            }
+            return null;
         });
+    }
+    public void SelectOptionsMenuInitial(int opNum){
+        switch (opNum) {
+            case 1:
+                CreateBookOptionUI();
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    public void CreateBookOptionUI(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===CREATE=BOOK===");
+        System.out.print("Enter the title of book: ");
+        String titleOfBook = sc.nextLine();
+        System.out.print("Enter the price of book: ");
+        int price = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter the year of book: ");
+        int year = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter the name o author: ");
+        String nameAuthor = sc.nextLine();
+        System.out.print("Enter the nationality o author: ");
+        String nationalityAuthor = sc.nextLine();
+
+        Author author = new Author(nameAuthor,nationalityAuthor);
+        bs.AddBook(titleOfBook, author, price, year);
+
+        System.out.println("Book created successfully!");
+        InitialMenu();
+        sc.close();
     }
 }
